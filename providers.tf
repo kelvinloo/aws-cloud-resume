54377a1fc412c28fcb54a3b91793280c8356186a
Kelvin <kelvinl.ctd@gmail.com>
2023-05-03 21:31:48 +1200

    removed credential references

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region     = var.aws_region
   //access_key = var.aws_access_key
   //secret_key = var.aws_secret_key
-  profile    = "resumeprofile"
+  //profile    = "resumeprofile"
 }
 
 // Additional provider configuration for US East 1 Certificate must be issued here for Cloudfront SSL
@@ -12,5 +12,5 @@ provider "aws" {
   region     = "us-east-1"
   //access_key = var.aws_access_key
   //secret_key = var.aws_secret_key
-  profile    = "resumeprofile"
+  //profile    = "resumeprofile"
 }
